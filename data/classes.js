6ce1b40f1ba27d219c1040a8ab0d4dabe1d9bafb
Astragg <95982273+Astragg@users.noreply.github.com>
2026-04-21 17:01:18 +0100

    Update classes.js

--- data/classes.js
+++ data/classes.js
@@ -1,1 +1,6 @@
-
+const CLASSES = {
+  Wizard: { hitDie: 6, primary: "int" },
+  Cleric: { hitDie: 8, primary: "wis" },
+  Fighter: { hitDie: 10, primary: "str" },
+  Rogue: { hitDie: 8, primary: "dex" }
+};
