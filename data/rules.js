221db0cd5d3901c6822f3e079ae3018ec8558590
Astragg <95982273+Astragg@users.noreply.github.com>
2026-04-22 01:17:30 +0100

    Delete data/rules.js

--- data/rules.js
+++
@@ -1,6 +0,0 @@
-const RULES = [
-  { name:"Grappled", desc:"Speed becomes 0." },
-  { name:"Prone", desc:"Disadvantage on attacks." },
-  { name:"Advantage", desc:"Roll twice, take higher." },
-  { name:"Disadvantage", desc:"Roll twice, take lower." }
-];
