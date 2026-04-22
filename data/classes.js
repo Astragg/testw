5e3454b26408ef30ecd674e3352d07c83e230c3b
Astragg <95982273+Astragg@users.noreply.github.com>
2026-04-22 01:17:24 +0100

    Delete data/classes.js

--- data/classes.js
+++
@@ -1,6 +0,0 @@
-const CLASSES = {
-  Wizard: { hitDie: 6, primary: "int" },
-  Cleric: { hitDie: 8, primary: "wis" },
-  Fighter: { hitDie: 10, primary: "str" },
-  Rogue: { hitDie: 8, primary: "dex" }
-};
